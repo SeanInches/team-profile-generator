8f2b99d8921ad7675cbf12603ee1a0ae080e8581
Sean <seanminches@gmail.com>
2022-02-17 17:00:15 -0500

    workin on debugging

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const { inherits } = require("util");
 const employee = require("./lib/employee");
 const engineer = require("./lib/engineer");
 const intern = require("./lib/intern");
@@ -16,130 +15,136 @@ const employeeRole = {
   choices: ["Manager", "Engineer", "Intern"],
 };
 
-const managerRole = {
-  type: "input",
-  name: "managerName",
-  message: "Managers name?",
-  validate: function (answer) {
-    if (answer.length > 1) {
-      return console.log("Please enter a name.");
-    }
-    return true;
-  },
-  type: "input",
-  name: "managerID",
-  message: "What is the managers ID?",
-  validate: function (answer) {
-    if (answer.length < 1) {
-      return console.log("Please enter an ID");
-    }
-    return true;
-  },
+const managerRole = [
+  {
+    type: "input",
+    name: "managerName",
+    message: "Managers name?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter a name.");
+      }
+      return true;
+    },
+    type: "input",
+    name: "managerID",
+    message: "What is the managers ID?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter an ID");
+      }
+      return true;
+    },
 
-  type: "input",
-  name: "managerNumber",
-  message: "What is their office number?",
-  validate: function (answer) {
-    if (answer.length > 1) {
-      return console.log("Please enter an office number.");
-    }
-    return true;
-  },
+    type: "input",
+    name: "managerNumber",
+    message: "What is their office number?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter an office number.");
+      }
+      return true;
+    },
 
-  type: "input",
-  name: "managerEmail",
-  message: "What is their email?",
-  validate: function (answer) {
-    if (answer.length > 1) {
-      return console.log("Please enter an email address.");
-    }
-    return true;
+    type: "input",
+    name: "managerEmail",
+    message: "What is their email?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter an email address.");
+      }
+      return true;
+    },
   },
-};
+];
 
-const engineerRole = {
-  type: "input",
-  name: "engineerName",
-  message: "What is the engineers name?",
-  validate: function (answer) {
-    if (answer.length > 1) {
-      return console.log("Please enter a name");
-    }
-    return true;
-  },
+const engineerRole = [
+  {
+    type: "input",
+    name: "engineerName",
+    message: "What is the engineers name?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter a name");
+      }
+      return true;
+    },
 
-  type: "input",
-  name: "engineerID",
-  message: "What is the engineers ID?",
-  validate: function (answer) {
-    if (answer.length > 1) {
-      return console.log("Please enter an ID");
-    }
-    return true;
-  },
+    type: "input",
+    name: "engineerID",
+    message: "What is the engineers ID?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter an ID");
+      }
+      return true;
+    },
 
-  type: "input",
-  name: "engineerEmail",
-  message: "What is the engineers email?",
-  validate: function (answer) {
-    if (answer.length > 1) {
-      return console.log("Please enter an email");
-    }
-    return true;
-  },
+    type: "input",
+    name: "engineerEmail",
+    message: "What is the engineers email?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter an email");
+      }
+      return true;
+    },
 
-  type: "input",
-  name: "engineerGithub",
-  message: "What is the engineers github?",
-  validate: function (answer) {
-    if (answer.length > 1) {
-      return console.log("Please enter a github name");
-    }
-    return true;
+    type: "input",
+    name: "engineerGithub",
+    message: "What is the engineers github?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter a github name");
+      }
+      return true;
+    },
   },
-};
+];
 
-const internRole = {
-  type: "input",
-  name: "internName",
-  message: "What is the interns name?",
-  validate: function (answer) {
-    if (answer.length > 1) {
-      return console.log("Please enter a name");
-    }
-    return true;
-  },
+const internRole = [
+  {
+    type: "input",
+    name: "internName",
+    message: "What is the interns name?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter a name");
+      }
+      return true;
+    },
 
-  type: "input",
-  name: "internID",
-  message: "What is the interns ID?",
-  validate: function (answer) {
-    if (answer.length > 1) {
-      return console.log("Please enter an ID");
-    }
-    return true;
-  },
+    type: "input",
+    name: "internID",
+    message: "What is the interns ID?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter an ID");
+      }
+      return true;
+    },
 
-  type: "input",
-  name: "internEmail",
-  message: "What is the interns email?",
-  validate: function (answer) {
-    if (answer.length > 1) {
-      return console.log("Please enter an ID");
-    }
-    return true;
-  },
+    type: "input",
+    name: "internEmail",
+    message: "What is the interns email?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter an ID");
+      }
+      return true;
+    },
 
-  type: "input",
-  name: "internSchool",
-  message: "What school is the intern in?",
-  validate: function (answer) {
-    if (answer.length > 1) {
-      return console.log("Please enter a school");
-    }
-    return true;
+    type: "input",
+    name: "internSchool",
+    message: "What school is the intern in?",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter a school");
+      }
+      return true;
+    },
   },
-};
+];
 
 const managers = [];
 const engineers = [];
@@ -180,49 +185,49 @@ function init(response) {
         return;
       });
   }
-}
 
-if (response === "Engineer") {
-  inquirer
-    .prompt(engineerRole)
+  if (response === "Engineer") {
+    inquirer
+      .prompt(engineerRole)
 
-    .then((response) => {
-      const updateEngineer = new engineer(
-        response.engineerName,
-        response.engineerID,
-        response.engineerEmail,
-        response.engineerGithub
-      );
-      engineers.push(updateEngineer);
-      addNextemployee();
-      return;
-    })
+      .then((response) => {
+        const updateEngineer = new engineer(
+          response.engineerName,
+          response.engineerID,
+          response.engineerEmail,
+          response.engineerGithub
+        );
+        engineers.push(updateEngineer);
+        addNextemployee();
+        return;
+      })
 
-    .catch((error) => {
-      console.log(error);
-      return;
-    });
-}
-if (response === "Intern") {
-  inquirer
-    .prompt(internRole)
+      .catch((error) => {
+        console.log(error);
+        return;
+      });
+  }
+  if (response === "Intern") {
+    inquirer
+      .prompt(internRole)
 
-    .then((response) => {
-      const updateIntern = new intern(
-        response.internName,
-        response.internID,
-        response.internEmail,
-        response.internSchool
-      );
-      interns.push(updateIntern);
-      addNextemployee();
-      return;
-    })
+      .then((response) => {
+        const updateIntern = new intern(
+          response.internName,
+          response.internID,
+          response.internEmail,
+          response.internSchool
+        );
+        interns.push(updateIntern);
+        addNextemployee();
+        return;
+      })
 
-    .catch((error) => {
-      console.log(error);
-      return;
-    });
+      .catch((error) => {
+        console.log(error);
+        return;
+      });
+  }
 }
 
 function addNextemployee() {
