46000a44e452d7aba8639122c341c2d742edb4e9
Sean <seanminches@gmail.com>
2022-02-17 15:49:53 -0500

    made functions for employee card insert

--- src/teamgen.js
+++ src/teamgen.js
@@ -38,3 +38,44 @@ function generateEmployees(renderTeam) {
   }
   return renderGroup.join(``);
 }
+
+const renderManager = (Manager) => {
+  return `<div class="card h-100">
+    <div class="card-body">
+        <div class="text-center card-title">
+            <h1>${Manager.getRole()}<h1>
+                <h5 class="fw-bolder">${Manager.getName()}</h5>
+                <li class="list-group-item">ID #: ${Manager.getID()}</li>
+                <li class="list-group-item"><a class="list-font" href="mailto:${Manager.getEmail()}">${Manager.getEmail()}</a></li>
+                <li class="list-group-item"><a class="list-font" Office Number: ${Manager.getOfficenumber()}</a></li>
+        </div>
+    </div>
+</div>`;
+};
+
+const renderEngineer = (Engineer) => {
+  return `<div class="card h-100">
+    <div class="card-body">
+        <div class="text-center card-title">
+            <h1>${Engineer.getRole()}<h1>
+                <h5 class="fw-bolder">${Engineer.getName()}</h5>
+                <li class="list-group-item">ID #: ${Engineer.getID()}</li>
+                <li class="list-group-item"><a class="list-font" href="mailto:${Engineer.getEmail()}">${Engineer.getEmail()}</a></li>
+                <li class="list-group-item"><a class="list-font" href="${Engineer.getGithub()}">${Engineer.getGithub()}</a></li>
+        </div>
+    </div>
+</div>`;
+};
+
+const renderIntern = (Intern) => {
+  return `<div class="card h-100">
+    <div class="card-body">
+        <div class="text-center card-title">
+            <h1>${Intern.getRole()}<h1>
+                <h5 class="fw-bolder">${Intern.getRole()}</h5>
+                <li class="list-group-item">ID #: ${Intern.getID()}</li>
+                <li class="list-group-item"><a class="list-font" href="${Intern.getEmail()}">${Intern.getEmail()}</a></li>
+                <li class="list-group-item list-font">${Intern.getSchool()}</li>
+        </div>
+    </div>`;
+};
