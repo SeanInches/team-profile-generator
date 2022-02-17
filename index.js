ec479bc922ab313f0f7cd2c990c94db99af04335
Sean <seanminches@gmail.com>
2022-02-17 17:10:22 -0500

    halp

--- index.js
+++ index.js
@@ -20,41 +20,41 @@ const managerRole = [
     type: "input",
     name: "managerName",
     message: "Managers name?",
-    validate: function (answer) {
-      if (answer.length < 1) {
-        return console.log("Please enter a name.");
-      }
-      return true;
-    },
+    // validate: function (answer) {
+    //   if (answer.length < 1) {
+    //     return console.log("Please enter a name.");
+    //   }
+    //   return true;
+    // },
     type: "input",
     name: "managerID",
     message: "What is the managers ID?",
-    validate: function (answer) {
-      if (answer.length < 1) {
-        return console.log("Please enter an ID");
-      }
-      return true;
-    },
+    // validate: function (answer) {
+    //   if (answer.length < 1) {
+    //     return console.log("Please enter an ID");
+    //   }
+    //   return true;
+    // },
 
     type: "input",
     name: "managerNumber",
     message: "What is their office number?",
-    validate: function (answer) {
-      if (answer.length < 1) {
-        return console.log("Please enter an office number.");
-      }
-      return true;
-    },
+    // validate: function (answer) {
+    //   if (answer.length < 1) {
+    //     return console.log("Please enter an office number.");
+    //   }
+    //   return true;
+    // },
 
     type: "input",
     name: "managerEmail",
     message: "What is their email?",
-    validate: function (answer) {
-      if (answer.length < 1) {
-        return console.log("Please enter an email address.");
-      }
-      return true;
-    },
+    // validate: function (answer) {
+    //   if (answer.length < 1) {
+    //     return console.log("Please enter an email address.");
+    //   }
+    //   return true;
+    // },
   },
 ];
 
